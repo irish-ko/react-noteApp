8273bce78ff71d3700370622e578f5d413e9f898
irish-ko <95221600+irish-ko@users.noreply.github.com>
2022-01-21 23:14:57 +0200

    add post add post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import "./style/App.css";
 
 export default function App() {
-  const [state, changeState] = useState(1);
   const [value, changeVal] = useState("Write text");
 
   return (
     <div className="App">
-      <h1>{value}</h1>
+      {/* <h1>{value}</h1>
       <input
         type="text"
         value={value}
         onChange={(e) => changeVal(e.target.value)}
       />
       <hr />
+      <ClassCounter/>
+      <ClassCounter/>
+      <ClassCounter/>
+      <hr />
 
       <Counter />
       <Counter />
       <Counter />
       <Counter />
-      <Counter />
+      <Counter /> */}
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div className="">Javascript - programming languages!</div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
